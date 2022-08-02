7d0aeabbbbab3881714335dda6feec3386b6d6e6
Yin.Miya <yinyanming@spromise.com>
2022-08-02 14:37:15 +0800

    更新打包固件脚本，更新测试案例打包

--- Packet_Release_msi.bat
+++ Packet_Release_msi.bat
@@ -1,5 +1,14 @@
+echo off
 set DEVENV="E:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.com"
 %DEVENV% FactoryAutoTest.sln /Rebuild "Release_console|x64" /project FactoryAutoTest
+if not %errorlevel% == 0 ( goto ERROR_OUT )
 %DEVENV% FactoryAutoTest.sln /Rebuild "Release|x64" /project FactoryAutoTest
+if not %errorlevel% == 0 ( goto ERROR_OUT )
 %DEVENV% FactoryAutoTest.sln /Rebuild "Release" /project SetupFactoryAutoTest
+if not %errorlevel% == 0 ( goto ERROR_OUT )
+goto END
+:ERROR_OUT
+echo make fail
+goto END
+:END
 pause
